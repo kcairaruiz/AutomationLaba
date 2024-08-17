eaa907ae23c66b18a58edacf8fe7fe7c9fafd6af
kcairaruiz <karencaru@gmail.com>
2024-08-17 20:21:47 -0300

    git remote add origin https://github.com/kcairaruiz/LabaAutomationCourse.git git branch -M main git push -u origin main

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /*types of variables in Jave
+
+        * */
+        String firstName = "Solvd";
+        String lastName = "Nerd";
+
+        int num1 = 3;
+        int num2 = 5;
+
+
+        System.out.println("Hello and welcome!");
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(firstName + " " + lastName);
+
+
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num2 / num1);
+        System.out.println(num1 % num2);
+
+    }
+}
